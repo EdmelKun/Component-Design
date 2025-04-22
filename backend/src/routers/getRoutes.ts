0b14b905e22173b70fe07871030ac658ad903e25
Edmel John Linaugo <68092712+EdmelKun@users.noreply.github.com>
2025-04-22 19:27:59 +0800

    chore: modified backend to send json data

--- backend/src/routers/getRoutes.ts
+++ backend/src/routers/getRoutes.ts
@@ -1,9 +1,38 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World SE bleehehh");
+router.get("/", async (req, res) => {
+  const data = [
+    {
+      id: 1,
+      name: "John Doe",
+      age: 20,
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      age: 19,
+    },
+    {
+      id: 3,
+      name: "John Smith",
+      age: 56,
+    },
+    {
+      id: 4,
+      name: "Jane Smith",
+      age: 55,
+    },
+    {
+      id: 5,
+      name: "Johnny Bravo",
+      age: 20,
+    },
+  ];
+
+  res.json(data);
 });
 
 export default router;
